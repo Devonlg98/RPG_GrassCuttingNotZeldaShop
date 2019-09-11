839d10fd9ddec7932029da011bc04ee8fe41b5c4
Devon Gregory <Devonlg98@gmail.com>
2019-09-11 12:29:23 -0700

    test

---
+++ RPG_GrassCutting/RPG_GrassCutting/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_GrassCutting
+{
+    public class Inventory
+    {
+        public int price = 0;
+        public int healthGain = 0;
+        public int hungerGain = 0;
+        public string foodString;
+
+        public Inventory(int v1, int v2, int v3, string v4)
+        {
+            this.price = v1;
+            this.healthGain = v2;
+            this.hungerGain = v3;
+            this.foodString = v4;
+        }
+    }
+}
---
+++ RPG_GrassCutting/RPG_GrassCutting/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_GrassCutting
+{
+    class Player
+    {
+        public int rupees = 1;
+        public int startHealth = 10;
+        public int startHunger = 10;
+        public int hunger = 0;
+        public int health = 0;
+    }
+}
+
